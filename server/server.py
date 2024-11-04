db420cb3eaaba57e87d799097aae0ba4f5e983e2
RainMaker1707 <hadrien.allegaert@student.uclouvain.be>
2024-11-04 14:18:23 +0100

    Add first webpage base

--- server/server.py
+++ server/server.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="static", static_folder="styles")
 
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=["GET"])
+@app.route("/home", methods=["GET"])
+def home():
+    return render_template("home.html", 
+                        MENU=render_template("menubar.html", STYLE=url_for('static', filename="menubar.css")), 
+                        STYLE=url_for('static', filename="home.css"),
+                        COMMANDS=render_template("base.html", COMMANDS="LS"))
+
+
+
+
 @app.route("/login", methods=["GET", "POST"])
 @app.route("/logout", methods=["GET", "POST"])
 @app.route("/signin", methods=["GET", "POST"])
@@ -14,7 +23,7 @@ app = Flask(__name__)
 @app.route("/profile/<user_id>", methods=["GET"])
 @app.route("/forum/post", methods=["GET", "POST"])
 @app.route("/forum/post/<post_id>", methods=["GET", "POST"])
-def home():
+def test():
     return "Hello World!"
 
 
