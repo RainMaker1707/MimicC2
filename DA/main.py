b4b656f732c1d3bd2df9f37aaa3326b8312e8343
RainMaker1707 <hadrien.allegaert@student.uclouvain.be>
2025-01-13 17:43:14 +0100

    DA

---
+++ DA/main.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+
+# script for data augmentation
+def replacer(text, target, replacement):
+    pass
+
+
+def save_file(text):
+    pass
+
+
+def read_file(path):
+    lines = ""
+    with open(path, 'r') as file:
+        lines = file.read()
+    return lines
+
+
+
+if __name__ == "__main__":
+    print("Data Augmentation script.")
